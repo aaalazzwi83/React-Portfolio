4bd5d70e852d09d6515fb53dccdc8d6a3aa9c213
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-05-08 11:48:17 -0700

    Updates

---
+++ precache-manifest.42b353966a2bfa56289080e71746aa69.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f67c84143633773c21a28d434f3614a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ee61985eb0feae7b4da7",
+    "url": "/React-Portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c4448009a94cb313293a",
+    "url": "/React-Portfolio/static/css/main.e438005a.chunk.css"
+  },
+  {
+    "revision": "ee61985eb0feae7b4da7",
+    "url": "/React-Portfolio/static/js/2.4a21fa7f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/React-Portfolio/static/js/2.4a21fa7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4448009a94cb313293a",
+    "url": "/React-Portfolio/static/js/main.4221e8ed.chunk.js"
+  },
+  {
+    "revision": "efc0de8cbd9e4d420dc8",
+    "url": "/React-Portfolio/static/js/runtime-main.52b30c36.js"
+  },
+  {
+    "revision": "63c26a7a0f2d8077a9ef99e0317ab8d0",
+    "url": "/React-Portfolio/static/media/about-me-photo.63c26a7a.jpg"
+  },
+  {
+    "revision": "db4b7f96de39cccba9dcbe9cfcd66cdd",
+    "url": "/React-Portfolio/static/media/bamazon.db4b7f96.jpg"
+  },
+  {
+    "revision": "9afd4004291fbe39699bd94bda3b4c13",
+    "url": "/React-Portfolio/static/media/budebackground.9afd4004.jpg"
+  },
+  {
+    "revision": "0ab325b1628724c10698afe3fc606a2a",
+    "url": "/React-Portfolio/static/media/clicky.0ab325b1.png"
+  },
+  {
+    "revision": "6729261fe563d4e81e875c4f9a3c86e4",
+    "url": "/React-Portfolio/static/media/crystalcollector.6729261f.png"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/React-Portfolio/static/media/ep_naturalblack.23e2b6cf.png"
+  },
+  {
+    "revision": "7db626bf664c1799fb90c846d73ef7d7",
+    "url": "/React-Portfolio/static/media/fridgechef.7db626bf.png"
+  },
+  {
+    "revision": "e841e204e154c8e262e57a4003c73833",
+    "url": "/React-Portfolio/static/media/friendFinder.e841e204.png"
+  },
+  {
+    "revision": "b3d0d3169ddb829e68440fb066d765c2",
+    "url": "/React-Portfolio/static/media/fureverHome.b3d0d316.png"
+  },
+  {
+    "revision": "4c530ae74d437b3f96ba9326d6512cb8",
+    "url": "/React-Portfolio/static/media/giftastic.4c530ae7.png"
+  },
+  {
+    "revision": "c5ae42126f301be8f25810e945c2593f",
+    "url": "/React-Portfolio/static/media/googlebooks.c5ae4212.png"
+  },
+  {
+    "revision": "3b7f2a598d390d925f797e2a75a071c0",
+    "url": "/React-Portfolio/static/media/liri-node-app.3b7f2a59.png"
+  },
+  {
+    "revision": "9f3422b4ca4284c4a9fb223d428521f3",
+    "url": "/React-Portfolio/static/media/reelTime.9f3422b4.png"
+  },
+  {
+    "revision": "27d8bdb03161e0dc83091362e56d0c24",
+    "url": "/React-Portfolio/static/media/traintimes.27d8bdb0.png"
+  },
+  {
+    "revision": "8a13f78d9decede47bb5d93af4d0e67d",
+    "url": "/React-Portfolio/static/media/trivia.8a13f78d.png"
+  }
+]);
