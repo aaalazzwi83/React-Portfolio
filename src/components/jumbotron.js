65bda0a81d186503d8c966e54cee16a507728978
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-05-08 21:43:32 -0700

    test

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -11,8 +11,8 @@ const MainSection = (props) => {
             <div className="col-12">
                 <Jumbotron fluid>
                     <Container fluid>
-                    <h1 className="display-3">From Full English to Full Stack</h1>
-                    <p className="lead">My personal journey from chef to coder.</p>
+                    <h1 className="display-3">From the BA to Full Stack Developer</h1>
+                    <p className="lead">My personal journey from a Business Analyst to coder.</p>
 
                     <a className="logo" href="https://github.com/aaalazzwi83">
                         <img src={githubLogo} alt="GitHub logo" id="github" />
@@ -34,8 +34,8 @@ const MainSection = (props) => {
                 </div>
                 <div className="col-12">
                     <div id="aboutMeText">
-                        <p>As an Englishman who's spent most of his career in the catering industry, I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin, I am now proficient in React, Javascript, Node.js, Express, HTML & CSS, among other programs, libraries and frameworks.</p>
-                        <p>With a keen eye for designing front-end and a logical brain for tackling back-end, I'd be an essential asset to your team.</p>
+                        <p>Experienced Senior Information Technology Business Analyst with a demonstrated history of working in the building materials industry. Skilled in Visio, Requirements Analysis, Quality Assurance, Business Analysis, and Consulting. Strong information technology professional with a Bachelor's degree focused in Business Administration and Management, General from Alpetra University . , I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin, I am now proficient in React, Javascript, Node.js, Express, HTML & CSS, among other programs, libraries and frameworks.</p>
+                        <p>With a sharp eye for designing front-end and a logical brain for tackling back-end, I'd be an essential asset to your team.</p>
                         <p>Feel free to peruse my favorite projects via the navbar above, or even visit my GitHub to see my full catalogue. I'd love to hear from you, too! Fire me an email from the contact page.</p>
                     </div>
                 </div>
